cb0dcd3eccae954473e1cd99ae3c2a5b6e1b2539
Tim <ptimothyp@gmail.com>
2022-08-09 06:16:41 +1000

    Facy cup.

--- cup.hs
+++ cup.hs
@@ -1,10 +1,28 @@
-cup f10z = \message -> message f10z
-cup' f10z message = message f10z
+cup flOz = \message -> message flOz
+cup' flOz message = message flOz
 
-cup6 = cup 10
-cup6' = cup' 12
-cup12 = cup 14
+cup6 = cup 6
+cup6' = cup' 6
+cup12 = cup 12
 
-getz cup = cup (\f10z -> f10z)
+getOz cup = cup (\flOz -> flOz)
+getz' cup = cup id
 
+drink aCup ozDrank
+  | ozLeft < 0 = cup 0
+  | otherwise = cup ozLeft
+  where
+    flOz = getz' aCup
+    ozLeft = flOz - ozDrank
+
+drink' aCup ozDrank = if ozDiff >= 0
+                      then cup ozDiff
+                      else cup 0
+   where flOz = getOz aCup
+         ozDiff = flOz - ozDrank
+
+afterBigGulp = drink' cup6 10
+
+afterSomeDrinks = getOz (foldl drink cup6 [1,4,1])
+afterFewDrinks = getOz (foldr (flip drink) cup6 [1,4,1])
 
