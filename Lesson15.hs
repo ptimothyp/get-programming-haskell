ffac80a8de9e4fc070f3ad55e66228d66e4a3ea1
Timothy <ptimothyp@gmail.com>
2022-08-18 07:09:54 +1000

    Bits encrypt.

--- Lesson15.hs
+++ Lesson15.hs
@@ -66,3 +66,24 @@ rotDecoder vals = map rotCharDecoder vals
   where
     sizeOfAlphabet = fromEnum (maxBound:: Char) + 1
     rotCharDecoder = rotNdecoder sizeOfAlphabet
+
+xorBool :: Bool -> Bool -> Bool
+xorBool value1 value2 = (value1 || value2) && (not (value1 && value2))
+
+xorPair :: (Bool, Bool) -> Bool
+xorPair (v1, v2) = xorBool v1 v2
+
+xor :: [Bool] -> [Bool] -> [Bool]
+xor list1 list2 = map xorPair (zip list1 list2)
+
+type Bits = [Bool]
+intToBits' :: Int -> Bits
+intToBits' 0 = [False]
+intToBits' 1 = [True]
+intToBits' n = if (remainder == 0)
+                  then False : intToBits' nextVal
+                  else True : intToBits' nextVal
+  where 
+    remainder = n `mod` 2
+    nextVal = n `div` 2
+
