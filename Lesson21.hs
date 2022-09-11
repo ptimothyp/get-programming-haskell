0e55a386a117635689c778ba35c0647643024b7f
Tim <ptimothyp@gmail.com>
2022-09-12 00:14:42 +1000

    Fix merge issues.

--- Lesson21.hs
+++ Lesson21.hs
@@ -1,21 +1,10 @@
 import Distribution.TestSuite (TestInstance(name))
 helloPerson :: String -> String
-<<<<<<< HEAD
 helloPerson name = "Hello" ++ " " ++ name ++ "!"
 
 main :: IO ()
 main = do
   putStrLn "Hello! What is your name?"
   name <- getLine
-  let statement =   helloPerson name
-=======
-helloPerson name = "Hello" ++ " " ++ name ++ "!!"
-
-
-main :: IO ()
-main = do
-  putStr  "Hello! What's your name? "
-  name <- getLine
   let statement = helloPerson name
->>>>>>> d7166ed186a4488d463c849a59cbda103af381d2
   putStrLn statement
